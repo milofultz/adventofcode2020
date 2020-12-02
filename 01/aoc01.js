0836fd096bc401f723a5661a604ab0b22b4be603
Milo Fultz <milopfultz@gmail.com>
2020-12-01 21:19:20 -0800

    01 Refactor JS Solution

--- 01/aoc01.js
+++ 01/aoc01.js
@@ -1,32 +1,24 @@
 import givenNums from '/aoc01-data.js';
 
-function findTwoNumbersThatSumTo2020(numbers) {
-  let output = '';
+function findNumbersThatSumTo2020(numbers) {
+  let twoAnswer, threeAnswer;
   for (let i = 0; i < numbers.length; i++) {
     for (let j = i; j < numbers.length; j++) {
-      output =
-        numbers[i] + numbers[j] === 2020 ? numbers[i] * numbers[j] : output;
-    }
-  }
-  return output;
-}
-
-function findThreeNumbersThatSumTo2020(numbers) {
-  let output = '';
-  for (let i = 0; i < numbers.length; i++) {
-    for (let j = i; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === 2020) {
+        twoAnswer = numbers[i] * numbers[j];
+      }
       for (let k = j; k < numbers.length; k++) {
-        output =
-          numbers[i] + numbers[j] + numbers[k] === 2020
-            ? numbers[i] * numbers[j] * numbers[k]
-            : output;
+        if (numbers[i] + numbers[j] + numbers[k] === 2020) {
+          threeAnswer = numbers[i] * numbers[j] * numbers[k];
+        }
       }
     }
   }
-  return output;
+  return [twoAnswer, threeAnswer];
 }
 
-console.log('Two Numbers Solution: ' + findTwoNumbersThatSumTo2020(givenNums));
+const [twoAnswer, threeAnswer] = findNumbersThatSumTo2020(givenNums);
 console.log(
-  'Three Numbers Solution: ' + findThreeNumbersThatSumTo2020(givenNums)
+  `Two Numbers Solution: ${twoAnswer}\n` +
+    `Three Numbers Solution: ${threeAnswer}`
 );
