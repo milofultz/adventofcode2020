6a30d7140a0265f8c35786cee7180a650fb37f66
Milo Fultz <milopfultz@gmail.com>
2020-12-01 22:28:44 -0800

    01 Refactor Python solution

--- 01/aoc01.py
+++ 01/aoc01.py
@@ -1,7 +1,6 @@
 def main():
     given_nums = make_list_of_given_nums()
-    print(find_two_numbers_sum_of_2020(given_nums))
-    print(find_three_numbers_sum_of_2020(given_nums))
+    print(find_products_of_sums_of_2020(given_nums))
 
 
 def make_list_of_given_nums():
@@ -12,21 +11,16 @@ def make_list_of_given_nums():
     return data
 
 
-def find_two_numbers_sum_of_2020(numbers: list) -> int:
-    for i, num1 in enumerate(numbers):
-        for num2 in numbers[i:]:
-            if num1 + num2 == 2020:
-                return num1 * num2
-    return -1
-
-
-def find_three_numbers_sum_of_2020(numbers: list) -> int:
+def find_products_of_sums_of_2020(numbers: list) -> int:
+    two, three = 0, 0
     for i, num1 in enumerate(numbers):
         for j, num2 in enumerate(numbers[i:]):
+            if num1 + num2 == 2020:
+                two = num1 * num2
             for num3 in numbers[j:]:
                 if num1 + num2 + num3 == 2020:
-                    return num1 * num2 * num3
-    return -1
+                    three = num1 * num2 * num3
+    return two, three
 
 
 if __name__ == '__main__':
