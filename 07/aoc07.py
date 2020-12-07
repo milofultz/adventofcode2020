b43af4dad22fa56eab20647b79c3b6a81fec57d1
Milo Fultz <milopfultz@gmail.com>
2020-12-06 23:53:39 -0800

    Refactor to OOP

--- 07/aoc07.py
+++ 07/aoc07.py
@@ -1,6 +1,42 @@
 PUZZLE_INPUT = "aoc07-data"
 
 
+class Bags:
+    def __init__(self, bag_contents):
+        self.bag_contents = bag_contents
+
+    def get_bags_containing_color(self, desired_color: str) -> set:
+        matches = set()
+        for entry in self.bag_contents.keys():
+            if desired_color in self.get_names_of_bags_contained_in(entry):
+                matches.add(entry)
+        return matches
+
+    def get_names_of_bags_contained_in(self, color: str) -> set:
+        contents = set()
+        current_bag_contents = self.bag_contents.get(color)
+        if len(current_bag_contents) is None:
+            return contents
+        else:
+            for bag in current_bag_contents:
+                contents.add(bag)
+                contents.update(self.get_names_of_bags_contained_in(bag))
+            return contents
+
+    def get_number_of_bags_inside(self, color: str) -> int:
+        contents = 0
+        current_bag_contents = self.bag_contents.get(color)
+        if len(current_bag_contents) is None:
+            return contents
+        else:
+            for bag, quantity in current_bag_contents.items():
+                bags_inside = self.get_number_of_bags_inside(bag)
+                for i in range(quantity):
+                    # current bag plus amount of all bags inside
+                    contents += 1 + bags_inside
+            return contents
+
+
 def get_all_bag_contents() -> dict:
     with open(PUZZLE_INPUT, 'r') as f:
         bags = f.read().split('\n')
@@ -12,53 +48,19 @@ def get_all_bag_contents() -> dict:
     return all_bags
 
 
-def parse_inner_bags(bags: str) -> dict:
-    bags = bags[:-1].replace(' bags', '').replace(' bag', '').split(', ')
+def parse_inner_bags(inner_bags: str) -> dict:
+    inner_bags = inner_bags[:-1].replace(' bags', '').replace(' bag', '').split(', ')
     contents = dict()
-    for inner_bag in bags:
+    for inner_bag in inner_bags:
         quantity, color = inner_bag.split(' ', 1)
         if quantity.isnumeric():
             contents[color] = int(quantity)
     return contents
 
 
-def get_bags_containing_color(desired_color: str, lookup: dict) -> set:
-    matches = set()
-    for entry in lookup.keys():
-        if desired_color in get_names_of_bags_contained_in(entry, lookup):
-            matches.add(entry)
-    return matches
-
-
-def get_names_of_bags_contained_in(color: str, lookup: dict) -> set:
-    contents = set()
-    current_bag_contents = lookup.get(color)
-    if len(current_bag_contents) is None:
-        return contents
-    else:
-        for bag in current_bag_contents:
-            contents.add(bag)
-            contents.update(get_names_of_bags_contained_in(bag, lookup))
-        return contents
-
-
-def get_number_of_bags_inside(color: str, lookup: dict) -> int:
-    contents = 0
-    current_bag_contents = lookup.get(color)
-    if len(current_bag_contents) is None:
-        return contents
-    else:
-        for bag, quantity in current_bag_contents.items():
-            bags_inside = get_number_of_bags_inside(bag, lookup)
-            for i in range(quantity):
-                # current bag plus amount of all bags inside
-                contents += 1 + bags_inside
-        return contents
-
-
 if __name__ == "__main__":
-    bag_contents = get_all_bag_contents()
+    bags = Bags(get_all_bag_contents())
     print("Bags that eventually contain at least one shiny gold bag: ", end='')
-    print(len(get_bags_containing_color('shiny gold', bag_contents)))
+    print(len(bags.get_bags_containing_color('shiny gold')))
     print("Bags that are required to be inside one shiny gold bag: ", end='')
-    print(get_number_of_bags_inside('shiny gold', bag_contents))
+    print(bags.get_number_of_bags_inside('shiny gold'))
