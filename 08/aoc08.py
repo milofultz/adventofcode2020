512455fb89fcda32369eeefe593a73bb278cf89f
Milo Fultz <milopfultz@gmail.com>
2020-12-07 22:17:55 -0800

    Refactor solution

--- 08/aoc08.py
+++ 08/aoc08.py
@@ -4,77 +4,62 @@ import copy
 PUZZLE_INPUT = 'aoc08-data'
 
 
-def get_instructions():
+def get_instructions() -> list:
     with open(PUZZLE_INPUT, 'r') as f:
         data = f.read().split('\n')
-    instructions = list()
+    output = list()
     for line in data:
         instruction = dict()
         inst_type, val = line.split(' ')
         val = int(val)
         instruction['type'] = inst_type
         instruction['val'] = val
-        instructions.append(instruction)
-    return instructions
+        output.append(instruction)
+    return output
 
 
 def get_accumulator_on_infinite_loop(instructions: list) -> int:
-    visited_instructions = set()
-    accumulator = 0
-    instruction_index = 0
-    while instruction_index not in visited_instructions:
-        visited_instructions.add(instruction_index)
-        current_instruction = instructions[instruction_index]
-        if current_instruction['type'] == 'acc':
-            accumulator += current_instruction['val']
-            instruction_index += 1
-        elif current_instruction['type'] == 'jmp':
-            instruction_index += current_instruction['val']
-        elif current_instruction['type'] == 'nop':
-            instruction_index += 1
-        else:
-            print('ELSE ERROR in loop')
-            instruction_index += 1
-    return accumulator
+    result = get_accumulator_and_completion_type(instructions)
+    return result['accumulator'] if result['end_type'] == 'loop' else None
 
 
-def find_corrupted_instruction(instructions) -> int:
-    number_of_instructions = len(instructions)
-    def get_accumulator_if_uncorrupted_instruction(new_instructions):
-        visited_instructions = set()
-        accumulator = 0
-        instruction_index = 0
-        while instruction_index not in visited_instructions:
-            visited_instructions.add(instruction_index)
-            current_instruction = new_instructions[instruction_index]
-            if current_instruction['type'] == 'acc':
-                accumulator += current_instruction['val']
-                instruction_index += 1
-            elif current_instruction['type'] == 'jmp':
-                instruction_index += current_instruction['val']
-            elif current_instruction['type'] == 'nop':
-                instruction_index += 1
-            else:
-                print('ELSE ERROR in loop')
-                instruction_index += 1
-            if instruction_index == number_of_instructions:
-                return accumulator
-        return None
+def get_accumulator_with_fixed_instruction(instructions) -> int:
     for index, instruction in enumerate(instructions):
-        instructions_copy = copy.deepcopy(instructions)
-        instruction_copy = copy.deepcopy(instruction)
         if instruction['type'] in ['nop', 'jmp']:
-            instruction_copy['type'] = 'jmp' if instruction['type'] == 'nop' else 'nop'
-            instructions_copy[index] = instruction_copy
-            val = get_accumulator_if_uncorrupted_instruction(instructions_copy)
-            if val is not None:
-                return val
+            new_instructions = copy.deepcopy(instructions)
+            changed_instruction = copy.deepcopy(instruction)
+            changed_instruction['type'] = 'jmp' if instruction['type'] == 'nop' else 'nop'
+            new_instructions[index] = changed_instruction
+            result = get_accumulator_and_completion_type(new_instructions)
+            if result['end_type'] == 'complete':
+                return result['accumulator']
     return None
 
 
+def get_accumulator_and_completion_type(instructions: list) -> dict:
+    instructions_length = len(instructions)
+    indexes_visited = set()
+    accumulator = 0
+    current_index = 0
+    while current_index not in indexes_visited:
+        indexes_visited.add(current_index)
+        current_instruction = instructions[current_index]
+        if current_instruction['type'] == 'acc':
+            accumulator += current_instruction['val']
+            current_index += 1
+        elif current_instruction['type'] == 'jmp':
+            current_index += current_instruction['val']
+        elif current_instruction['type'] == 'nop':
+            current_index += 1
+        else:
+            print('ELSE ERROR in loop')
+            current_index += 1
+        if current_index == instructions_length:
+            return {'accumulator': accumulator, 'end_type': 'complete'}
+    return {'accumulator': accumulator, 'end_type': 'loop'}
 
 
 if __name__ == "__main__":
     instructions = get_instructions()
     print(get_accumulator_on_infinite_loop(instructions))
-    print(find_corrupted_instruction(instructions))
+    print(get_accumulator_with_fixed_instruction(instructions))
