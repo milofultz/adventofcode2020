4f11022505b58bdf6983d39ad2602076699ce961
Milo Fultz <milopfultz@gmail.com>
2020-12-07 21:57:44 -0800

    Solve part 2

--- 08/aoc08.py
+++ 08/aoc08.py
@@ -1,3 +1,6 @@
+import copy
+
+
 PUZZLE_INPUT = 'aoc08-data'
 
 
@@ -22,7 +25,6 @@ def get_accumulator_on_infinite_loop(instructions: list) -> int:
     while instruction_index not in visited_instructions:
         visited_instructions.add(instruction_index)
         current_instruction = instructions[instruction_index]
-        print(current_instruction['type'])
         if current_instruction['type'] == 'acc':
             accumulator += current_instruction['val']
             instruction_index += 1
@@ -36,6 +38,43 @@ def get_accumulator_on_infinite_loop(instructions: list) -> int:
     return accumulator
 
 
+def find_corrupted_instruction(instructions) -> int:
+    number_of_instructions = len(instructions)
+    def get_accumulator_if_uncorrupted_instruction(new_instructions):
+        visited_instructions = set()
+        accumulator = 0
+        instruction_index = 0
+        while instruction_index not in visited_instructions:
+            visited_instructions.add(instruction_index)
+            current_instruction = new_instructions[instruction_index]
+            if current_instruction['type'] == 'acc':
+                accumulator += current_instruction['val']
+                instruction_index += 1
+            elif current_instruction['type'] == 'jmp':
+                instruction_index += current_instruction['val']
+            elif current_instruction['type'] == 'nop':
+                instruction_index += 1
+            else:
+                print('ELSE ERROR in loop')
+                instruction_index += 1
+            if instruction_index == number_of_instructions:
+                return accumulator
+        return None
+    for index, instruction in enumerate(instructions):
+        instructions_copy = copy.deepcopy(instructions)
+        instruction_copy = copy.deepcopy(instruction)
+        if instruction['type'] in ['nop', 'jmp']:
+            instruction_copy['type'] = 'jmp' if instruction['type'] == 'nop' else 'nop'
+            instructions_copy[index] = instruction_copy
+            val = get_accumulator_if_uncorrupted_instruction(instructions_copy)
+            if val is not None:
+                return val
+    return None
+
+
+
+
 if __name__ == "__main__":
     instructions = get_instructions()
     print(get_accumulator_on_infinite_loop(instructions))
+    print(find_corrupted_instruction(instructions))
