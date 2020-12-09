80d989e26d6db79ac9207dcd6cb2d280bf3006db
Milo Fultz <milopfultz@gmail.com>
2020-12-08 23:29:15 -0800

    Refactor contiguous, names, and edge cases

--- 09/aoc09.py
+++ 09/aoc09.py
@@ -7,26 +7,26 @@ def get_port_output() -> list:
     return [int(line) for line in output]
 
 
-def get_first_non_sum_number(numbers: list, size_of_components: int) -> int:
-    start, current_index = 0, size_of_components
-    for number in numbers[size_of_components:]:
-        if not is_sum_of_components(number, numbers[start:current_index]):
+def get_first_non_sum_number(numbers: list, size_of_preamble: int) -> int:
+    start, current_index = 0, size_of_preamble
+    for number in numbers[size_of_preamble:]:
+        if not is_sum_of_numbers_in_preamble(number, numbers[start:current_index]):
             return number
         start += 1
         current_index += 1
     return None
 
 
-def is_sum_of_components(desired_sum, components):
-    for start, first_num in enumerate(components):
-        for second_num in components[start + 1:]:
+def is_sum_of_numbers_in_preamble(desired_sum, numbers):
+    for index, first_num in enumerate(numbers):
+        for second_num in numbers[index + 1:]:
             if first_num + second_num == desired_sum:
                 return True
     return False
 
 
 def get_encryption_weakness(target: int, numbers: list) -> int:
-    for index in range(2, len(numbers)):
+    for index in range(len(numbers)):
         result = get_contiguous_summands_of_target(target, numbers[:index])
         if result is not None:
             return min(result) + max(result)
@@ -34,24 +34,21 @@ def get_encryption_weakness(target: int, numbers: list) -> int:
 
 
 def get_contiguous_summands_of_target(target: int, summands: list) -> list:
-    first_number_index = len(summands) - 1
-    while first_number_index != 1:
-        first_number = summands[first_number_index]
+    if len(summands) < 2:
+        return None
+    for first_number_index, first_number in enumerate(summands):
         if first_number > target:
             continue
         contiguous_sum = first_number
         contiguous_summands = [first_number]
-        second_number_index = first_number_index - 1
-        while second_number_index != 0:
-            second_number = summands[second_number_index]
+        second_number_index = first_number_index + 1
+        for second_number in summands[second_number_index:]:
             contiguous_sum += second_number
             contiguous_summands.append(second_number)
             if contiguous_sum > target:
                 break
             elif contiguous_sum == target:
                 return contiguous_summands
-            second_number_index -= 1
-        first_number_index -= 1
     return None
 
 
