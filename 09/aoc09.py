b4b9b30b18f57bf3771faddd24866af1c9af849d
Milo Fultz <milopfultz@gmail.com>
2020-12-08 22:26:26 -0800

    Solve part 2

--- 09/aoc09.py
+++ 09/aoc09.py
@@ -25,6 +25,31 @@ def is_sum_of_components(desired_sum, components):
     return False
 
 
+def get_encryption_weakness(target: int, numbers: list) -> int:
+    for index in range(2, len(numbers)):
+        result = get_contiguous_summands_of_target(target, numbers[:index])
+        if result is not None:
+            return min(result) + max(result)
+    return None
+
+
+def get_contiguous_summands_of_target(target: int, summands: list) -> list:
+    for start, first_number in enumerate(summands):
+        resulting_summands = [first_number]
+        too_big = False
+        for second_number in summands[start+1:]:
+            if not too_big:
+                resulting_summands.append(second_number)
+                temp_sum = sum(resulting_summands)
+                if temp_sum == target:
+                    return resulting_summands
+                elif temp_sum > target:
+                    too_big = True
+    return None
+
+
 if __name__ == "__main__":
     data = get_data()
-    print(get_first_non_sum_number(data, 25))
+    target = get_first_non_sum_number(data, 25)
+    print(target)
+    print(get_encryption_weakness(target, data))
