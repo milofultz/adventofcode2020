62708d1a9ac4d5cff75c47fd92dd15369d2a43b0
Milo Fultz <milopfultz@gmail.com>
2020-12-09 21:50:19 -0800

    Remove comments

--- 10/aoc10.py
+++ 10/aoc10.py
@@ -1,6 +1,3 @@
-# in: list of numbers
-# out: int (# of 1 jolt * # of 3-jolt)
-
 PUZZLE_INPUT = 'aoc10-data'
 
 
@@ -11,30 +8,21 @@ def get_adapter_list():
 
 
 def get_adapter_differences(adapters: list) -> dict:
-    # sort list of adapters
     sorted_adapters = sorted(adapters)
-    # create diff dict with 1, 2, and 3 set to 0
     differences = {
         '1': 0,
         '2': 0,
         '3': 1
     }
-    # set current adapter to first item in list
     last_adapter = 0
-    # for adapter in adapters starting on item 2
     for adapter in sorted_adapters:
-        # get diff and increment at key
         difference = adapter - last_adapter
         differences[str(difference)] += 1
         last_adapter = adapter
-    # return dict
     return differences
 
 
 if __name__ == "__main__":
-    # get data
     adapter_list = get_adapter_list()
-    # get dict of all adapter differences
     difference_dict = get_adapter_differences(adapter_list)
-    # return number of 1j * number of 3J
     print(difference_dict['1'] * difference_dict['3'])
