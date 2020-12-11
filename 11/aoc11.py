f85b37e441cfb9955b53fdbe0b749a50a3858366
Milo Fultz <milopfultz@gmail.com>
2020-12-10 22:47:41 -0800

    Remove comments and rewrite good ones

--- 11/aoc11.py
+++ 11/aoc11.py
@@ -17,12 +17,6 @@ DIRECTIONS = [
 ]
 
 
-
-# ## Parser
-#
-# IN: Empty layout of seats (string)
-# OUT: Parsed seats layout
-#
 def get_seat_layout() -> list:  # list of lists
     with open(PUZZLE_INPUT, 'r') as f:
         data = f.read().split('\n')
@@ -32,57 +26,22 @@ def get_seat_layout() -> list:  # list of lists
     return output
 
 
-# ## Program
-#
-# IN: Parsed seats layout
-# OUT: number of occupied seats (int)
-#
-# Rules:
-# - If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied.
-# - If a seat is occupied (#) and four or more seats adjacent to it are also occupied, the seat becomes empty.
-# - Otherwise, the seat's state does not change.
-#
-# How to store the seats layout?
-# - List of lists
-#     - high amount of overhead needed to constantly search the lists
-#     - empty seats take up lots of space in list
-#     - easy to check adjacent seats
-# - hash table with coords as keys
-#     - fast search
-#     - no empty seats/less space needed
-#     - harder to check adjacent seats
-#
-# Naive solution - List of lists
-# FUNC: Get settled seats
-# IN: Seat list (list of lists)
-# OUT: (modified copy of) Seat list (list of lists)
 def get_settled_layout(layout: list) -> list:
-    # - make deep copy of seat list to work with
     working_layout = copy.deepcopy(layout)
-    # - initial state is None
     last_layout = None
-    # - while initial state is not equal to seat list
     while last_layout != working_layout:
-    #     - initial state is deep copy of seat list
         last_layout = copy.deepcopy(working_layout)
-    #     - For row in seats
         for row_number, row in enumerate(last_layout):
-    #         - for each seat in row
             for col_number, seat in enumerate(row):
-    #             - if empty and can be occupied (func)
                 if seat == EMPTY and can_be_occupied_adjacent(last_layout, col_number, row_number):
-    #                 - set to occupied
                     working_layout[row_number][col_number] = OCCUPIED
-    #             - if occupied and can be emptied (func)
                 if seat == OCCUPIED and can_be_emptied_adjacent(last_layout, col_number, row_number):
-    #                 - set to empty
                     working_layout[row_number][col_number] = EMPTY
-    # - return modified copy of seat list
     return working_layout
 
 
-# - If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied.
 def can_be_occupied_adjacent(layout: list, x: int, y: int) -> bool:
+    """ Return True if no occupied seats are adjacent to it """
     x_range = y_range = [-1, 0, 1]
     if y == 0:
         y_range = [0, 1]
@@ -101,8 +60,8 @@ def can_be_occupied_adjacent(layout: list, x: int, y: int) -> bool:
     return True
 
 
-# - If a seat is occupied (#) and four or more seats adjacent to it are also occupied, the seat becomes empty.
 def can_be_emptied_adjacent(layout: list, x: int, y: int) -> bool:
+    """ Return True if four+ seats adjacent to coordinates are occupied """
     adjacent_occupied_seats = 0
     x_range = y_range = [-1, 0, 1]
     if y == 0:
@@ -124,16 +83,6 @@ def can_be_emptied_adjacent(layout: list, x: int, y: int) -> bool:
     return False
 
 
-# FUNC: Get occupied seats
-# IN: Seat list
-# OUT: Occupied seats (int)
-#
-# - set counter to 0
-# - For row in seats
-#     - For seat in row
-#         - if seat is occupied
-#             - counter += 1
-# - return counter
 def get_occupied_seats(layout: list) -> int:
     occupied_seats = 0
     for row in layout:
@@ -144,87 +93,50 @@ def get_occupied_seats(layout: list) -> int:
 
 
 def get_settled_layout_from_a_distance(layout: list) -> list:
-    # - make deep copy of seat list to work with
     working_layout = copy.deepcopy(layout)
-    # - initial state is None
     last_layout = None
-    # - while initial state is not equal to seat list
-    # layout_num = 0
     while last_layout != working_layout:
-    #     - initial state is deep copy of seat list
         last_layout = copy.deepcopy(working_layout)
-    #     - For row in seats
         for row_number, row in enumerate(last_layout):
-    #         - for each seat in row
             for col_number, seat in enumerate(row):
-    #             - if empty and can be occupied (func)
                 if seat == EMPTY and can_be_occupied_from_a_distance(last_layout, col_number, row_number):
-    #                 - set to occupied
                     working_layout[row_number][col_number] = OCCUPIED
-    #             - if occupied and can be emptied (func)
                 if seat == OCCUPIED and can_be_emptied_from_a_distance(last_layout, col_number, row_number):
-    #                 - set to empty
                     working_layout[row_number][col_number] = EMPTY
-    # - return modified copy of seat list
     return working_layout
 
 
-# FUNC: Can be occupied from a distance
-# IN: Seat list, X, Y
-# OUT: Boolean
 def can_be_occupied_from_a_distance(layout: list, x: int, y: int) -> bool:
-    # - for direction in visible_directions
+    """ Return True if no seats visible via directions are occupied """
     for direction in DIRECTIONS:
-    #     - set x and y to given coords
-    #     - add direction[x] to x
-    #     - add direction[y] to y
         x_sight, y_sight = x + direction['x'], y + direction['y']
-    #     - while 0 < x < length of row
-    #       and 0 < y < length of seat list:
         while (0 <= x_sight < len(layout[0]) and
                0 <= y_sight < len(layout)):
-    #         - if x,y is occupied
             visible_square = layout[y_sight][x_sight]
             if visible_square == OCCUPIED:
-    #             - return False
                 return False
             elif visible_square == EMPTY:
                 break
             x_sight, y_sight = x_sight + direction['x'], y_sight + direction['y']
-    # - return True
     return True
 
 
-# FUNC: Can be emptied from a distance
-# IN: Seat list, X (int), Y (int), tolerance (int)
-# OUT: Boolean
 def can_be_emptied_from_a_distance(layout: list, x: int, y: int) -> bool:
-    # - set counter to 0
+    """ Return True if five or more seats visible via directions are occupied """
     visible_occupied_seats = 0
-    # - for direction in visible_directions
     for direction in DIRECTIONS:
-    #     - set x and y to given coords
-    #     - add direction[x] to x
-    #     - add direction[y] to y
         x_sight, y_sight = x + direction['x'], y + direction['y']
-    #     - while 0 < x < length of row
-    #       and 0 < y < length of seat list:
         while (0 <= x_sight < len(layout[0]) and
                0 <= y_sight < len(layout)):
-    #         - if x,y is occupied
             visible_square = layout[y_sight][x_sight]
             if visible_square == OCCUPIED:
-    #             - add 1 to counter
                 visible_occupied_seats += 1
-    #             - if counter >= tolerance
                 if visible_occupied_seats >= 5:
-    #                 - return True
                     return True
                 break
             if visible_square == EMPTY:
                 break
             x_sight, y_sight = x_sight + direction['x'], y_sight + direction['y']
-    # - return False
     return False
 
 
