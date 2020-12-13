e472205d6c6d541cdcde1a79ba162c1e42a680cc
Milo Fultz <milopfultz@gmail.com>
2020-12-12 21:56:22 -0800

    Solve part 1

--- 13/aoc13.py
+++ 13/aoc13.py
@@ -3,14 +3,13 @@ PUZZLE_INPUT_SMALL = "aoc13-data-small"
 
 
 def get_arrival_time_and_ids():
-    with open(PUZZLE_INPUT_SMALL, "r") as f:
-        arrival, ids = f.read().split("\n")
+    with open(PUZZLE_INPUT, "r") as f:
+        arrival, ids = f.read().strip().split("\n")
     return int(arrival), [int(bus_id) for bus_id in ids.split(",") if bus_id != "x"]
 
 
 def part_1_solution(arrival: int, bus_ids: list) -> int:
     earliest_bus = get_earliest_bus_info(arrival, bus_ids)
-    print(earliest_bus)
     return earliest_bus["id"] * earliest_bus["wait"]
 
 
@@ -21,7 +20,7 @@ def get_earliest_bus_info(arrival: int, bus_ids: list) -> dict:
         this_bus = {"id": bus_id,
                     "time": (prev_bus_time + 1) * bus_id,
                     "wait": bus_id - minutes_before}
-        print(bus_id, this_bus)
+        # print(bus_id, this_bus)
         if this_bus["wait"] < earliest_bus["wait"]:
             earliest_bus = this_bus
     return earliest_bus
