de462e1cde5fe46a11c9972389de177f4b909d25
Milo Fultz <milopfultz@gmail.com>
2020-12-15 18:13:03 -0800

    Solve part 2

--- 14/aoc14.py
+++ 14/aoc14.py
@@ -3,6 +3,7 @@ import re
 
 P_IN = "aoc14-data"
 P_IN_S = "aoc14-data-small"
+P_IN_S2 = "aoc14-data-small2"
 
 
 def parse_data():
@@ -35,37 +36,66 @@ def parse_data():
     return output
 
 
-def get_sum_of_all_masked_values(mask_groups: list) -> None:
-    #   IN: dict of mask and key:values for each
-    #   OUT: int, sum of all masked values
-    # * create masked values dict
+def get_sum_of_all_masked_values(mask_groups: list) -> int:
     masked_values = dict()
-    # * for each new bitmask in list
     for mask in mask_groups:
-        # * for each memory location and value in the bitmask dict
         for loc, value in mask['data'].items():
-            # * convert the value to binary string
             bin_value = bin(value)[2:].zfill(36)
-            # * for each char in both the mask and the binary string:
             for index in range(36):
-                # * if the mask char is a 0, change that binary char to a 0
                 if mask['mask'][index] == '0':
                     bin_value = bin_value[:index] + '0' + bin_value[index + 1:]
-                # * if the mask char is an 1, change that binary char to a 1
                 elif mask['mask'][index] == '1':
                     bin_value = bin_value[:index] + '1' + bin_value[index + 1:]
-            # * add that int of string to the memory location of the masked values dict
             masked_values[loc] = int(bin_value, 2)
-    # * create sum of 0
-    sum = 0
-    # * for each key in masked values
+    masked_values_sum = 0
     for loc in masked_values:
-        # * add value to sum
-        sum += masked_values[loc]
-    # * return sum
-    return sum
+        masked_values_sum += masked_values[loc]
+    return masked_values_sum
 
+
+def get_sum_of_all_masked_values_part_2(mask_groups: list) -> int:
+    masked_values = dict()
+    for mask in mask_groups:
+        for loc, value in mask['data'].items():
+            # make binary location
+            bin_loc = bin(loc)[2:].zfill(36)
+            # for each char in the loc and the mask
+            for index in range(36):
+                # if the mask char is a 0, do nothing
+                if mask['mask'][index] == '0':
+                    pass
+                # if the mask char is a 1, change loc at index to 1
+                elif mask['mask'][index] == '1':
+                    bin_loc = bin_loc[:index] + '1' + bin_loc[index + 1:]
+                # else, if 'x'
+                else:
+                    # change loc at index to 'x'
+                    bin_loc = bin_loc[:index] + 'X' + bin_loc[index + 1:]
+            memory_locs = get_all_locs(bin_loc)
+            for new_loc in memory_locs:
+                masked_values[int(new_loc, 2)] = value
+    masked_values_sum = 0
+    for loc in masked_values:
+        masked_values_sum += masked_values[loc]
+    return masked_values_sum
+
+
+def get_all_locs(binary: str) -> list:
+    ### edge cases
+    ### base case
+    # if no X, return the string in an array
+    if 'X' not in binary:
+        return [binary]
+    ### recursive case
+    # else, if 1 or more X
+    else:
+        locs = list()
+        # locs += get_all_locs(binary.replace('X', '1', 1))
+        locs += get_all_locs(binary.replace('X', '0', 1))
+        locs += get_all_locs(binary.replace('X', '1', 1))
+        return locs
 
 if __name__ == "__main__":
     masks = parse_data()
-    print(get_sum_of_all_masked_values(masks))
+    # print(get_sum_of_all_masked_values(masks))
+    print(get_sum_of_all_masked_values_part_2(masks))
