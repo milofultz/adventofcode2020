8aa78216d606a4ed0d652b8579f42a10fe476525
Milo Fultz <milopfultz@gmail.com>
2020-12-15 23:23:51 -0800

    Refactor to simpler data structure and less code

--- 15/aoc15.py
+++ 15/aoc15.py
@@ -1,36 +1,27 @@
+# Much simpler/faster refactor inspired by Scarygami:
+# https://github.com/Scarygami/aoc2020/blob/main/15/day15.py
+
+
 P_IN = "19,0,5,1,10,13"
 P_INS = "1,3,2"
 P_EX = "0,3,6"
 
 
 def get_number_at_turn(final_turn: int, starting_numbers: list) -> int:
-    last_seen = dict()
-    turn_counter = 0
-    for number in starting_numbers:
-        # print(f"Starting: {number}")
-        last_seen[number] = {"last": turn_counter, "first": -1}
-        turn_counter += 1
+    last_seen = {value: index for index, value in enumerate(starting_numbers)}
     previous_number = starting_numbers[-1]
-    while turn_counter < final_turn:
-        if last_seen[previous_number]["first"] == -1:
-            # print(f"Seen once: {previous_number}; New number: 0")
-            last_seen[0]["first"], last_seen[0]["last"] = (
-                last_seen[0]["last"], turn_counter)
-            previous_number = 0
+    for turn in range(len(starting_numbers), final_turn):
+        if previous_number not in last_seen:
+            current_number = 0
         else:
-            current_number = last_seen[previous_number]["last"] - last_seen[previous_number]["first"]
-            # print(f"Seen: {previous_number}; New number: {current_number}")
-            if not last_seen.get(current_number):
-                last_seen[current_number] = {"last": turn_counter, "first": -1}
-            else:
-                last_seen[current_number]["first"], last_seen[current_number]["last"] = (
-                    last_seen[current_number]["last"], turn_counter)
-            previous_number = current_number
-        turn_counter += 1
+            current_number = (turn - 1) - last_seen[previous_number]
+        last_seen[previous_number] = turn - 1
+        previous_number = current_number
+        turn += 1
     return previous_number
 
 
 if __name__ == "__main__":
     starting_numbers = [int(num) for num in P_IN.split(",")]
-    print(get_number_at_turn(2020, starting_numbers))
-    print(get_number_at_turn(30000000, starting_numbers))
+    print(f"Part 1: {get_number_at_turn(2020, starting_numbers)}")
+    print(f"Part 2: {get_number_at_turn(30000000, starting_numbers)}")
