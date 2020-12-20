b0692c856110b930b7ee5ba1267fa6bff3b46511
Milo Fultz <milopfultz@gmail.com>
2020-12-19 21:11:12 -0800

    Simplify data parsing

--- 10/aoc10.py
+++ 10/aoc10.py
@@ -4,11 +4,14 @@ PUZZLE_INPUT = 'aoc10-data'
 def get_adapter_list():
     with open(PUZZLE_INPUT, 'r') as f:
         adapter_list = f.read().split('\n')
-    return sorted([int(adapter) for adapter in adapter_list])
+    adapter_list = sorted([int(adapter) for adapter in adapter_list])
+    # Add built in device adapter
+    adapter_list.append(adapter_list[-1] + 3)
+    return adapter_list
 
 
 def get_adapter_differences(adapters: list) -> dict:
-    differences = {1: 0, 2: 0, 3: 1}
+    differences = {1: 0, 2: 0, 3: 0}
     last_adapter = 0
     for adapter in adapters:
         differences[adapter - last_adapter] += 1
