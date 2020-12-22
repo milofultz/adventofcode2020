cc92b409fda41fb4ed9a9372609fb0d6a14da1f5
Milo Fultz <milopfultz@gmail.com>
2020-12-22 14:47:58 -0800

    Parse equation with one parentheses

--- 18/aoc18.py
+++ 18/aoc18.py
@@ -8,18 +8,39 @@ def get_equations(fp):
         data = f.read().replace(' ', '').split('\n')
     output = []
     for line in data:
-        equation = []
-        number = ""
-        for char in line:
-            if char.isnumeric():
-                number += char
-            else:
-                equation.append(int(number))
-                equation.append(char)
-                number = ""
-        output.append(equation)
+        output.append(parse_equation(line))
     return output
+
+
+def parse_equation(raw_eq):
+    equation = []
+    number = ""
+    index = 0
+    while index < len(raw_eq):
+        if raw_eq[index].isnumeric():
+            number += raw_eq[index]
+        elif raw_eq[index] in ["+", "*"]:
+            equation.append(int(number))
+            equation.append(raw_eq[index])
+            number = ""
+        elif raw_eq[index] == "(":
+            sub_eq = ""
+            parentheses_count = 1
+            index += 1
+            while parentheses_count != 0:
+                print(raw_eq[index])
+                if raw_eq[index] == "(":
+                    parentheses_count += 1
+                elif raw_eq[index] == ")":
+                    parentheses_count -= 1
+                sub_eq += raw_eq[index] if parentheses_count != 0 else ""
+                index += 1
+            equation.append(parse_equation(sub_eq))
+        index += 1
+    equation.append(number)
+    return equation
 
 
 if __name__ == "__main__":
     equations = get_equations(P_INS2)
+    print(equations)
