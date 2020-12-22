df5037ed62a9f5f57979d862d94638175e83d7eb
Milo Fultz <milopfultz@gmail.com>
2020-12-22 14:30:33 -0800

    Parse most basic input

--- 18/aoc18.py
+++ 18/aoc18.py
@@ -1,16 +1,25 @@
 P_IN = "aoc18-data"
 P_INS = "aoc18-data-small"
+P_INS2 = "aoc18-data-smaller"
 
 
 def get_equations(fp):
     with open(fp, 'r') as f:
         data = f.read().replace(' ', '').split('\n')
-    return data
-    # output = []
-    # number = ""
-    # for line in data:
-    #     for i in range(len(line)):
+    output = []
+    for line in data:
+        equation = []
+        number = ""
+        for char in line:
+            if char.isnumeric():
+                number += char
+            else:
+                equation.append(int(number))
+                equation.append(char)
+                number = ""
+        output.append(equation)
+    return output
 
 
 if __name__ == "__main__":
-    equations = get_equations(P_INS)
+    equations = get_equations(P_INS2)
