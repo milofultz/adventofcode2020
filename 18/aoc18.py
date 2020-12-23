558a561e9a529a85fadab7587da9bb6176d2e895
Milo Fultz <milopfultz@gmail.com>
2020-12-22 16:35:25 -0800

    Refactor functions and name changes

--- 18/aoc18.py
+++ 18/aoc18.py
@@ -1,5 +1,4 @@
 import copy
-from typing import Callable
 
 
 P_IN = "aoc18-data"
@@ -11,10 +10,7 @@ OPERATORS = ["+", "*"]
 def get_equations(fp):
     with open(fp, 'r') as f:
         data = f.read().replace(' ', '').split('\n')
-    output = []
-    for line in data:
-        output.append(parse_equation(line))
-    return output
+    return [parse_equation(line) for line in data]
 
 
 def parse_equation(raw_eq: str) -> list:
@@ -33,14 +29,14 @@ def parse_equation(raw_eq: str) -> list:
             index += 1
         else:
             sub_eq = ""
-            parentheses_count = 1
+            parentheses_depth = 1
             index += 1
-            while parentheses_count != 0:
+            while parentheses_depth != 0:
                 if raw_eq[index] == "(":
-                    parentheses_count += 1
+                    parentheses_depth += 1
                 elif raw_eq[index] == ")":
-                    parentheses_count -= 1
-                sub_eq += raw_eq[index] if parentheses_count != 0 else ""
+                    parentheses_depth -= 1
+                sub_eq += raw_eq[index] if parentheses_depth != 0 else ""
                 index += 1
             equation.append(parse_equation(sub_eq))
     if number != "":
@@ -52,45 +48,32 @@ def sum_all_equations(equations: list) -> tuple:
     total_no_precedence = 0
     total_add_precedence = 0
     for equation in equations:
-        total_no_precedence += sum_equation_no_precedence(equation)
-        total_add_precedence += sum_equation_add_precedence(equation)
+        total_no_precedence += sum_equation(equation)
+        total_add_precedence += sum_equation(equation, True)
     return total_no_precedence, total_add_precedence
 
 
-def flatten_equation(nested_eq: list, callback: Callable[[list], int]) -> list:
-    flattened_eq = copy.deepcopy(nested_eq)
-    for index, element in enumerate(flattened_eq):
-        if type(element) == list:
-            flattened_eq[index] = callback(element)
-    return flattened_eq
+def sum_equation(eq: list, plus_precedence: bool = False) -> int:
+    def flatten_equation(nested_eq: list) -> list:
+        flattened_eq = copy.deepcopy(nested_eq)
+        for i, element in enumerate(flattened_eq):
+            if type(element) == list:
+                flattened_eq[i] = sum_equation(element, plus_precedence)
+        return flattened_eq
 
-
-def sum_equation_no_precedence(eq: list) -> int:
-    while any(isinstance(element, list) for element in eq):
-        eq = flatten_equation(eq, sum_equation_no_precedence)
+    eq = flatten_equation(eq)
+    while "+" in eq and plus_precedence:
+        plus_i = eq.index("+")
+        eq[plus_i - 1] = eq[plus_i - 1] + eq[plus_i + 1]
+        del eq[plus_i:plus_i + 2]
     result = eq[0]
-    index = 1
-    while index < len(eq):
-        if eq[index] == "+":
-            result += eq[index + 1]
-        else:  # "*"
-            result *= eq[index + 1]
-        index += 2
-    return result
-
-
-def sum_equation_add_precedence(eq: list) -> int:
-    while any(isinstance(element, list) for element in eq):
-        eq = flatten_equation(eq, sum_equation_add_precedence)
-    while "+" in eq:
-        index = eq.index("+")
-        eq[index - 1] = eq[index - 1] + eq[index + 1]
-        del eq[index:index + 2]
-    result = eq[0]
-    index = 1
-    while index < len(eq):
-        result *= eq[index + 1]
-        index += 2
+    operator_i = 1
+    while operator_i < len(eq):
+        if eq[operator_i] == "*":
+            result *= eq[operator_i + 1]
+        else:  # "+"
+            result += eq[operator_i + 1]
+        operator_i += 2
     return result
 
 
