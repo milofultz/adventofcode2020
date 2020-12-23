f0126425ba9d6cb925dbf6bcd7ccda46e2960b92
Milo Fultz <milopfultz@gmail.com>
2020-12-22 22:44:37 -0800

    Get all valid outputs

--- 19/aoc19.py
+++ 19/aoc19.py
@@ -1,3 +1,6 @@
+import copy
+
+
 P_IN = "aoc19-data"
 P_INS = "aoc19-data-small"
 
@@ -8,11 +11,43 @@ def parse_data(fp) -> (dict, list):
     rules_dict = dict()
     for rule in raw_rules.split('\n'):
         order, nums = rule.split(": ")
-        rules_dict[order] = [group.replace("\"", "")
+        rules_dict[order] = [group.replace("\"", "").split(' ')
                              for group in nums.split(" | ")]
     return rules_dict, raw_messages.split('\n')
 
 
+def get_outputs(rule_number: str, rules: dict) -> set:
+    # * create valid set
+    outputs = set()
+    # * for rule group in group
+    for rule_group in rules[rule_number]:
+        # * create output array with empty string inside
+        output = [""]
+        # * for rule in rule group
+        for rule in rule_group:
+            # * if rule is "a" or "b"
+            if rule in ["a", "b"]:
+                # * make output array = element + ("a" or "b") for each element in output
+                output = [element + rule for element in output]
+            # * else
+            else:
+                # * set result to func(rules[rule])
+                result = get_outputs(rule, rules)
+                # * create temporary array
+                temp = list()
+                # * for element in output array
+                for element in output:
+                    # * for addition in result
+                    for addition in result:
+                        # * add element + addition to temporary array
+                        temp.append(element + addition)
+                output = copy.deepcopy(temp)
+        # * add all of output array to valid set
+        outputs.update(output)
+    # * return valid set
+    return outputs
+
+
 if __name__ == "__main__":
     rules, messages = parse_data(P_INS)
-    print(rules, messages)
+    valid = get_outputs("0", rules)
