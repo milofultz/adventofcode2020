7e8baf526413aa20406c8ddcb7b64e4d1dcfdea6
Milo Fultz <milopfultz@gmail.com>
2020-12-25 07:40:51 -0800

    Refactor out the edge lookup

--- 20/aoc20.py
+++ 20/aoc20.py
@@ -26,7 +26,7 @@ def parse_array(raw: str) -> np.array:
     return np.array(array)
 
 
-def get_corners_and_edges(tileset: dict) -> (list, list):
+def make_edge_lookup(tileset: dict) -> dict:
     edge_lookup = defaultdict(list)
     for tile_id, tile in tileset.items():
         current_tile = deepcopy(tile)
@@ -35,21 +35,26 @@ def get_corners_and_edges(tileset: dict) -> (list, list):
             edge_lookup[edge].append(tile_id)
             edge_lookup[edge[::-1]].append(tile_id)
             current_tile = np.rot90(current_tile)
+    return edge_lookup
+
+
+def get_corner_and_edge_ids(edges: dict) -> (list, list):
     edge_count = defaultdict(int)
     # Edges are a unique side that are only found on one tile
-    for id_list in edge_lookup.values():
+    for id_list in edges.values():
         if len(id_list) == 1:
             edge_count[id_list[0]] += 1
     # Corners have two unique sides (four if flipped)
-    corners = [num for num in edge_count.keys() if edge_count[num] == 4]
-    edges = [num for num in edge_count.keys() if num not in corners]
-    return corners, edges
+    edge_ids = [num for num in edge_count.keys() if edge_count[num] >= 2]
+    corner_ids = [num for num in edge_ids if edge_count[num] > 2]
+    return corner_ids, edge_ids
 
 
 if __name__ == "__main__":
     # Part 1
     tileset = parse_data(P_IN)
-    corner_ids, edge_ids = get_corners_and_edges(tileset)
+    edge_lookup = make_edge_lookup(tileset)
+    corner_ids, edge_ids = get_corner_and_edge_ids(edge_lookup)
     print(np.product(corner_ids))
     # Part 2
     inside_ids = [num for num in tileset.keys()
