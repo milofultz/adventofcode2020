71f01366b4f8418f1db1c576ff42eb7c380206b6
Milo Fultz <milopfultz@gmail.com>
2020-12-24 18:10:48 -0800

    Solve part 1

--- 20/aoc20.py
+++ 20/aoc20.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+from copy import deepcopy
 import numpy as np
 
 
@@ -11,7 +13,7 @@ def parse_data(fp: str) -> dict:
     output = dict()
     for entry in data:
         key, value = entry.split("\n", 1)
-        key = key[5:-1]
+        key = int(key[5:-1])
         value = parse_array(value)
         output[key] = value
     return output
@@ -24,5 +26,26 @@ def parse_array(raw: str) -> np.array:
     return np.array(array)
 
 
+def get_corners_and_edges(tileset: dict) -> (list, list):
+    edge_lookup = defaultdict(list)
+    for tile_id, tile in tileset.items():
+        current_tile = deepcopy(tile)
+        for i in range(4):
+            edge = "".join(x for x in current_tile[0])
+            edge_lookup[edge].append(tile_id)
+            edge_lookup[edge[::-1]].append(tile_id)
+            current_tile = np.rot90(current_tile)
+    edge_count = defaultdict(int)
+    for side, tile_ids in edge_lookup.items():
+        if len(tile_ids) == 1:
+            edge_count[tile_ids[0]] += 1
+    corners = [num for num in edge_count.keys() if edge_count[num] == 4]
+    edges = [num for num in edge_count.keys() if num not in corners]
+    return corners, edges
+
+
 if __name__ == "__main__":
-    tileset = parse_data(P_INS)
+    # Part 1
+    tileset = parse_data(P_IN)
+    corner_ids, edge_ids = get_corners_and_edges(tileset)
+    print(np.product(corner_ids))
