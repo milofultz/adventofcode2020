dd51844604569af8ff058a45eb26ed933953c0f8
Milo Fultz <milopfultz@gmail.com>
2021-07-18 09:34:50 -0700

    Convert to normal list from deque

--- 22/aoc22.py
+++ 22/aoc22.py
@@ -1,5 +1,3 @@
-from collections import deque
-from copy import deepcopy
 from time import time
 
 P_IN = './aoc22-data'
@@ -14,26 +12,22 @@ def get_decks():
     deck_1, deck_2 = raw_decks.replace('Player 1:\n', '').split('\n\nPlayer 2:\n')
     deck_1 = [int(x) for x in deck_1.split('\n') if x != '']
     deck_2 = [int(x) for x in deck_2.split('\n') if x != '']
-    return deque(deck_1), deque(deck_2)
+    return deck_1, deck_2
 
 
 def combat(deck_1, deck_2, recursive=False):
-    """ Play the game and return winner plus their deck """
+    """ Play the game and return winner plus their score """
     history = []
     while len(deck_1) > 0 and len(deck_2) > 0:
         if has_been_played(deck_1, history):
             return 1, deck_1
-        history.append(deepcopy(deck_1))
-        card_1 = deck_1.popleft()
-        card_2 = deck_2.popleft()
+        history.append(deck_1[:])
+        card_1 = deck_1.pop(0)
+        card_2 = deck_2.pop(0)
 
         if recursive and len(deck_1) >= card_1 and len(deck_2) >= card_2:
-            new_deck_1 = deepcopy(deck_1)
-            new_deck_2 = deepcopy(deck_2)
-            while len(new_deck_1) != card_1:
-                new_deck_1.pop()
-            while len(new_deck_2) != card_2:
-                new_deck_2.pop()
+            new_deck_1 = deck_1[:card_1]
+            new_deck_2 = deck_2[:card_2]
 
             winner, _ = combat(new_deck_1, new_deck_2, recursive=True)
             if winner == 1:
@@ -49,9 +43,13 @@ def combat(deck_1, deck_2, recursive=False):
             deck_2.append(card_2)
             deck_2.append(card_1)
 
-    winner = 1 if len(deck_1) != 0 else 2
-    winning_deck = deck_1 if len(deck_1) != 0 else deck_2
-    return winner, winning_deck
+    if len(deck_1) != 0:
+        winner = 1
+        winning_deck = deck_1
+    else:
+        winner = 2
+        winning_deck = deck_2
+    return winner, get_score(winning_deck)
 
 
 def has_been_played(deck_1, history):
@@ -66,28 +64,23 @@ def get_score(deck):
     """ Get the winner's deck score """
     total_score = 0
     while len(deck) != 0:
-        top_card = deck.popleft()
+        top_card = deck.pop(0)
         total_score += top_card * (len(deck) + 1)
 
     return total_score
 
 
-def problem_1():
+def problem(recursive=False):
+    start = time()
     deck_1, deck_2 = get_decks()
-    _, winning_deck = combat(deck_1, deck_2)
-    print(get_score(winning_deck))
-
-
-def problem_2():
-    deck_1, deck_2 = get_decks()
-    _, winning_deck = combat(deck_1, deck_2, recursive=True)
-    print(get_score(winning_deck))
+    winner, score = combat(deck_1, deck_2, recursive)
+    print(f"Winner: {winner}\nScore: {score}")
+    print(time() - start)
 
 
 if __name__ == "__main__":
-    start_1 = time()
-    problem_1()
-    print(time() - start_1)
-    start_2 = time()
-    problem_2()
-    print(time() - start_2)
+    # Problem 1
+    problem()
+
+    # Problem 2
+    problem(recursive=True)
