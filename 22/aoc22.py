87d574d2b4f66056c820780b003441b85df10d69
Milo Fultz <milopfultz@gmail.com>
2021-07-18 09:00:37 -0700

    Solve 22

---
+++ 22/aoc22.py
@@ -0,0 +1,89 @@
+from collections import deque
+from copy import deepcopy
+
+P_IN = './aoc22-data'
+P_INS = './aoc22-data-small'
+
+
+def get_decks():
+    """ Make both the decks from the input file """
+    with open(P_IN, 'r') as f:
+        raw_decks = f.read()
+
+    raw_decks = raw_decks.replace('Player 1:\n', '')
+    deck_1, deck_2 = raw_decks.split('\n\nPlayer 2:\n')
+    deck_1 = [int(x) for x in deck_1.split('\n') if x != '']
+    deck_2 = [int(x) for x in deck_2.split('\n') if x != '']
+    return deque(deck_1), deque(deck_2)
+
+
+def combat(deck_1, deck_2, recursive=False):
+    """ Play the game and return winner plus their deck """
+    history = []
+    while len(deck_1) > 0 and len(deck_2) > 0:
+        if has_been_played(deck_1, deck_2, history):
+            return 1, deck_1
+        history.append([deepcopy(deck_1), deepcopy(deck_2)])
+        card_1 = deck_1.popleft()
+        card_2 = deck_2.popleft()
+
+        if recursive and len(deck_1) >= card_1 and len(deck_2) >= card_2:
+            new_deck_1 = deepcopy(deck_1)
+            new_deck_2 = deepcopy(deck_2)
+            while len(new_deck_1) != card_1:
+                new_deck_1.pop()
+            while len(new_deck_2) != card_2:
+                new_deck_2.pop()
+
+            winner, _ = combat(new_deck_1, new_deck_2, recursive=True)
+            if winner == 1:
+                deck_1.append(card_1)
+                deck_1.append(card_2)
+            else:
+                deck_2.append(card_2)
+                deck_2.append(card_1)
+        elif card_1 > card_2:
+            deck_1.append(card_1)
+            deck_1.append(card_2)
+        else:
+            deck_2.append(card_2)
+            deck_2.append(card_1)
+
+    winner = 1 if len(deck_1) != 0 else 2
+    winning_deck = deck_1 if len(deck_1) != 0 else deck_2
+    return winner, winning_deck
+
+
+def has_been_played(deck_1, deck_2, history):
+    """ Check if the current hand has been played """
+    for record in history:
+        if record[0] == deck_1 and record[1] == deck_2:
+            return True
+    return False
+
+
+def get_score(deck):
+    """ Get the winner's deck score """
+    total_score = 0
+    while len(deck) != 0:
+        top_card = deck.popleft()
+        total_score += top_card * (len(deck) + 1)
+
+    return total_score
+
+
+def problem_1():
+    deck_1, deck_2 = get_decks()
+    _, winning_deck = combat(deck_1, deck_2)
+    print(get_score(winning_deck))
+
+
+def problem_2():
+    deck_1, deck_2 = get_decks()
+    _, winning_deck = combat(deck_1, deck_2, recursive=True)
+    print(get_score(winning_deck))
+
+
+if __name__ == "__main__":
+    problem_1()
+    problem_2()
